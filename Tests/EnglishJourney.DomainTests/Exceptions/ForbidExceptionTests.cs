92d92e8723b4e0354adf829f71262d7c2d4a551d
Mateusz Grzegorzewski <mateuszgrzegorzewski@gazeta.pl>
2024-07-06 12:59:13 +0200

    Update unit tests for Domain layer

---
+++ Tests/EnglishJourney.DomainTests/Exceptions/ForbidExceptionTests.cs
@@ -0,0 +1,19 @@
+﻿using EnglishJourney.Domain.Exceptions;
+using Xunit;
+
+namespace EnglishJourney.DomainTests.Exceptions
+{
+    public class ForbidExceptionTests
+    {
+        [Fact]
+        public void NotFoundException_ShouldSetMessageCorrectly()
+        {
+            // act
+            var exception = new ForbidException();
+            var expectedMessage = "Exception of type 'EnglishJourney.Domain.Exceptions.ForbidException' was thrown.";
+
+            // assert
+            Assert.Equal(expectedMessage, exception.Message);
+        }
+    }
+}
